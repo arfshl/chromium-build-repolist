b625441cdd822b37eab8310994b84e520ff47c5e
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2026-06-30 16:31:30 +0700

    no /

--- scripts/chrome-offline-installers/fetch-release.sh
+++ scripts/chrome-offline-installers/fetch-release.sh
@@ -7,7 +7,7 @@ curl -O https://raw.githubusercontent.com/shawnkhall/Tools/main/ChromeDownloader
 pwsh ChromeDownloader.ps1 win 64 -os 11 -do info >> scripts/chrome-offline-installers/rawdata.txt
 pwsh ChromeDownloader.ps1 win 32 -os 10 -do info >> scripts/chrome-offline-installers/rawdata.txt
 pwsh ChromeDownloader.ps1 win 64 -os 7 -do info >> scripts/chrome-offline-installers/rawdata.txt
-pwsh ChromeDownloader.ps1 win 32 -os 7 -do info >> scriptschrome-offline-installers/rawdata.txt
+pwsh ChromeDownloader.ps1 win 32 -os 7 -do info >> scripts/chrome-offline-installers/rawdata.txt
 pwsh ChromeDownloader.ps1 win 32 -os 5.2 -do info >> scripts/chrome-offline-installers/rawdata.txt
 
 # delete leftovers
