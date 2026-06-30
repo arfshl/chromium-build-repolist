efb9864a9983b093416a33a85ec2d2b27a61ae29
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2026-06-30 16:30:35 +0700

    "no such file or directories"

--- scripts/chrome-offline-installers/fetch-release.sh
+++ scripts/chrome-offline-installers/fetch-release.sh
@@ -4,11 +4,11 @@
 curl -O https://raw.githubusercontent.com/shawnkhall/Tools/main/ChromeDownloader.ps1
 
 # fetch chrome release and print the output to rawdata.txt
-pwsh ChromeDownloader.ps1 win 64 -os 11 -do info >> chrome-offline-installers/rawdata.txt
-pwsh ChromeDownloader.ps1 win 32 -os 10 -do info >> chrome-offline-installers/rawdata.txt
-pwsh ChromeDownloader.ps1 win 64 -os 7 -do info >> chrome-offline-installers/rawdata.txt
-pwsh ChromeDownloader.ps1 win 32 -os 7 -do info >> chrome-offline-installers/rawdata.txt
-pwsh ChromeDownloader.ps1 win 32 -os 5.2 -do info >> chrome-offline-installers/rawdata.txt
+pwsh ChromeDownloader.ps1 win 64 -os 11 -do info >> scripts/chrome-offline-installers/rawdata.txt
+pwsh ChromeDownloader.ps1 win 32 -os 10 -do info >> scripts/chrome-offline-installers/rawdata.txt
+pwsh ChromeDownloader.ps1 win 64 -os 7 -do info >> scripts/chrome-offline-installers/rawdata.txt
+pwsh ChromeDownloader.ps1 win 32 -os 7 -do info >> scriptschrome-offline-installers/rawdata.txt
+pwsh ChromeDownloader.ps1 win 32 -os 5.2 -do info >> scripts/chrome-offline-installers/rawdata.txt
 
 # delete leftovers
 rm ChromeDownloader.ps1
