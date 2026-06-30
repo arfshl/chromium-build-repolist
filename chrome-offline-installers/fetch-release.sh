4a4b784e91803cd5c3d3ff8f8efe167337f0c946
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2026-06-30 16:20:49 +0700

    delete scripts after fetch

--- chrome-offline-installers/fetch-release.sh
+++ chrome-offline-installers/fetch-release.sh
@@ -3,10 +3,12 @@
 # Get ChromeDownloader.ps1
 curl -O https://raw.githubusercontent.com/shawnkhall/Tools/main/ChromeDownloader.ps1
 
-# Fetch chrome release and print the output to rawdata.txt
-
+# fetch chrome release and print the output to rawdata.txt
 pwsh ChromeDownloader.ps1 win 64 -os 11 -do info >> chrome-offline-installers/rawdata.txt
 pwsh ChromeDownloader.ps1 win 32 -os 10 -do info >> chrome-offline-installers/rawdata.txt
 pwsh ChromeDownloader.ps1 win 64 -os 7 -do info >> chrome-offline-installers/rawdata.txt
 pwsh ChromeDownloader.ps1 win 32 -os 7 -do info >> chrome-offline-installers/rawdata.txt
 pwsh ChromeDownloader.ps1 win 32 -os 5.2 -do info >> chrome-offline-installers/rawdata.txt
+
+# delete leftovers
+rm ChromeDownloader.ps1
